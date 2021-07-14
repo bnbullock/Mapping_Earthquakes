59a6ca5f4b3e94dcc4278bde6f043b42e3a9a304
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-07-14 03:22:02 -0400

    Update mapping points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,11 +5,29 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 24,
     accessToken: API_KEY
 });
 
+// We create the tile layer that will be the background of our map.
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//L.circle([34.0522, -118.2437], {
+//    radius: 10000,
+//    color: 'yellow',
+//    fillColor: '#f03',
+//    fillOpacity: 0.5
+//}).addTo(map);
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    color: 'yellow',
+    fillcolor: 'ffffa1'
+}).addTo(map);
